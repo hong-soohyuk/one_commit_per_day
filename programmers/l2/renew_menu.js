1ac1a2c5691c460ac1b970a7c8ce803233fb62dc
Soohyuk Hong <soohong@student.42seoul.kr>
2023-01-04 15:46:15 +0900

    solve: 메뉴 리뉴얼

--- programmers/l2/renew_menu.js
+++ programmers/l2/renew_menu.js
@@ -1,31 +1,32 @@
-const make_powerset = (str, max_depth) => {
-    let flags = new Array({length: order.length}, false);
-    const get_power_set = (depth, map, max_dep) => {
-        if (depth === max_dep) {
-            map.set()
-        } else {
-            
-        }
+const make_course = (map, order) => {
+    let strArr = order.split("");
+    let result = [''];
+    for (let i = 0; i < strArr.length; i++) {
+        let len = result.length;
+        for (let x = 0; x < len; x++)
+            result.push(result[x] + strArr[i]);
     }
-    dfs(0, map, order.length);
-}
-
-const make_course(map, size, orders) => {
-
-    
-    for (let order of orders){
-        if (order.length < size)
-            continue ;
-        else if (order.length == size)
-            //map.set(order, ++)
-        else
-            
-    }
+    result.filter(v => v.length != 0).forEach((val) => {
+        val = val.split("").sort().join("");
+        map.has(val) ? map.set(val, map.get(val) + 1) : map.set(val, 1);
+    });
 }
 
 function solution(orders, course) {
-    const map = new Map();
-    course.forEach((v) => make_course(map, v, orders))
-    map = new Map([...map.entries()].sort()); // key sort
-    return (Array.from(map.keys()));
+    let map = new Map();
+    let answer = [];
+    for (let order of orders)
+        make_course(map, order);
+
+    for (let num of course) {
+        if (map.size == 0)
+            break;
+        let only_num = new Map([...map].filter(([key, value]) => key.length == num));
+        let course_menu =[...only_num.entries()].sort((a, b) => b[1] - a[1]);
+        let max = Math.max(...only_num.values());
+        course_menu = course_menu.filter((v) => v[1] == max && v[1] > 1);
+        course_menu.forEach(v => answer.push(v[0]));
+    }
+    return answer.sort();
 }
+
