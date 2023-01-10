df186449def1c2869fdc06523d9997fb75aeb393
hong-soohyuk <63241308+hong-soohyuk@users.noreply.github.com>
2023-01-10 22:05:09 +0900

    solve: num 2

--- etc/big_num_card.java
+++ etc/big_num_card.java
@@ -3,88 +3,69 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+public class Solution {
+  private static String solve(String target, int small, int big) {
+    int target_length = target.length();
+    int digits[] = new int[target_length];
+    boolean fill_big = false;
+    int start_index = -1;
+    int i = -1;
+    StringBuilder answer = new StringBuilder();
 
-public class Main {
-    private static void fill_digits(int[] array, int size, int n)
-    {
-        int i;
-
-        i = size - 1;
-
-        while (n != 0)
-        {
-            array[i] = n % 10;
-            n /= 10;
-            --i;
+    while (++i < digits.length) {
+      int digit = target.charAt(i) - '0';
+      if (fill_big)
+        digits[i] = big;
+      else if (digit == big || digit == small)
+        digits[i] = digit == big ? big : small;
+      else if (digit > small) {
+        digits[i] = digit > big ? big : small;
+        fill_big = true;
+      } else {
+        int start_over = i - 1;
+        while (start_over > -1 && digits[start_over] != big)
+          start_over--;
+        if (start_over == -1) {
+          while (++start_over < target_length - 1)
+            answer.append(big);
+          return answer.length() == 0 ? "-1" : answer.toString();
+        } else {
+          digits[start_over] = small;
+          i = start_over;
         }
+        fill_big = true;
+      }
     }
-    private static int num_length(int n)
-    {
-        int length = 0;
+    int j = start_index;
+    if (j == target_length - 1)
+      return "-1";
+    while (++j < target_length)
+      answer.append(digits[j]);
+    j = 0;
+    while (j < answer.length() && answer.charAt(j) == '0')
+      j++;
+    if (j == answer.length())
+      return "-1";
+    return answer.substring(j).toString();
+  }
 
-        if (n == 0)
-            return 1;
-        while (n != 0)
-        {
-            n /= 10;
-            ++length;
-        }
-        return length;
+  public static void main(String[] args) throws IOException {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    int test_cases;
+    String target;
+    int small;
+    int big;
+    test_cases = Integer.parseInt(bufferedReader.readLine());
+    StringBuilder sb = new StringBuilder();
+    for (int test_num = 0; test_num < test_cases; test_num++) {
+      StringTokenizer row_col = new StringTokenizer(bufferedReader.readLine(), " ");
+      target = row_col.nextToken();
+      small = Integer.parseInt(row_col.nextToken());
+      big = Integer.parseInt(row_col.nextToken());
+      sb.append("#").append((test_num + 1)).append(" ");
+      sb.append(solve(target, small, big)).append("\n");
     }
-    private static int solve(int target, int small, int big)
-    {
-        int     digits[];
-        int     answer = 0;
-        boolean fill_big = false;
-        int     start_index= -1;
-
-        digits = new int[num_length(target)];
-        fill_digits(digits, digits.length, target);
-        int i = -1;
-        while (++i < digits.length)
-        {
-            if (fill_big)
-                digits[i] = big;
-            else if (digits[i] == big || digits[i] == small)
-                digits[i] = digits[i] == big ? big : small;
-            else if (digits[i] > small)
-            {
-                digits[i] = digits[i] > big ? big : small;
-                fill_big = true;
-            }
-            else
-            {
-                digits[i] = -1;
-                start_index = i;
-            }
-        }
-        int j = start_index;
-        while(++j < digits.length)
-        {
-            answer *= 10;
-            answer += digits[j];
-        }
-        return (answer != 0 ? answer : -1);
-    }
-
-    public static void  main(String[] args) throws IOException
-    {
-        BufferedReader  bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int             test_cases;
-        int             target;
-        int             small;
-        int             big;
-        int             answer;
-
-        test_cases = Integer.parseInt(bufferedReader.readLine());
-        for(int test_num = 0; test_num < test_cases; test_num++)
-        {
-            StringTokenizer row_col = new StringTokenizer(bufferedReader.readLine(), " ");
-            target = Integer.parseInt(row_col.nextToken());
-            small = Integer.parseInt(row_col.nextToken());
-            big = Integer.parseInt(row_col.nextToken());
-            answer = solve(target, small, big);
-            System.out.printf("#%d %d\n", test_num + 1, answer);
-        }
-    }
+    System.out.print(sb);
+  }
 }
+
