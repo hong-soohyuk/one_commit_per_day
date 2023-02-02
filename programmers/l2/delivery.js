c92ed9cd36ddd5d9aac58e10fe8f5a6fe1e5173f
Soohyuk Hong <soohong@student.42seoul.kr>
2023-02-02 18:09:59 +0900

    add: amazing code in 택배 배달

--- programmers/l2/delivery.js
+++ programmers/l2/delivery.js
@@ -38,3 +38,23 @@ function solution(cap, n, del, pick) {
 
     return answer;
 }
+//
+function solution(cap, n, deliveries, pickups) {
+    let answer = 0;
+    
+    let d = 0;
+    let p = 0;
+    for (let i = n - 1; i > -1; i--) {
+        d -= deliveries[i];
+        p -= pickups[i];
+        
+        let count = 0;
+        while (d < 0 || p < 0) {
+            d += cap;
+            p += cap;
+            count++;
+        }
+        answer += (i + 1) * 2 * count;
+    }
+    return answer;
+}
