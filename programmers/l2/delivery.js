6313881588778a963340481435d5a1b5ca961026
Soohyuk Hong <soohong@student.42seoul.kr>
2023-02-02 17:49:30 +0900

    add: 2023 kakao blind 택배 배달과 수거

---
+++ programmers/l2/delivery.js
@@ -0,0 +1,40 @@
+const update = (array, ti, total, cap) => {
+    let i = ti;
+    let truck = 0;
+    if (total == 0)
+        return (0);
+    while (i > -1 && truck !== cap) {
+        if (array[i] == 0) {
+            --i;
+            continue ;
+        }
+        truck += array[i];
+        if (truck > cap) {
+            array[i] = (truck - cap);
+            truck = cap; 
+        }
+        else
+            array[i] = 0;
+        --i;
+    }
+    return (total - truck);
+}
+
+function solution(cap, n, del, pick) {
+    let answer = 0;
+    let dTotal = del.reduce((acc, e) => acc + e, 0);
+    let pTotal = pick.reduce((acc, e) => acc + e, 0);
+    let ti = n - 1;
+    while (dTotal !== 0 || pTotal !== 0)
+    {
+        while (del[ti] === 0 && pick[ti] === 0)
+            --ti;
+        if (dTotal != 0) 
+            dTotal = update(del, ti, dTotal, cap);
+        if (pTotal !== 0) 
+            pTotal = update(pick, ti, pTotal, cap);
+        answer += ((ti + 1) << 1);
+    }
+
+    return answer;
+}
