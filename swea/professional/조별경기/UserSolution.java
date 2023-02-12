48bf86c2acc1b476482ef1d142392ae922466e22
hong-soohyuk <63241308+hong-soohyuk@users.noreply.github.com>
2023-02-12 23:33:03 +0900

    add 조별경기

---
+++ swea/professional/조별경기/UserSolution.java
@@ -0,0 +1,23 @@
+class UserSolution
+{
+    public void init(int N)
+    {
+
+    }
+
+    public void updateScore(int mWinnerID, int mLoserID, int mScore)
+    {
+
+    }
+
+    public void unionTeam(int mPlayerA, int mPlayerB)
+    {
+
+    }
+
+    public int getScore(int mID)
+    {
+        return 0;
+    }
+}
+
