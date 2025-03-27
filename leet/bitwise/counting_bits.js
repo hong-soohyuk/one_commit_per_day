5792b54997482d1173c1a7d99f0dee71d00ef52e
hongsoohyuk2 <63241308+hong-soohyuk@users.noreply.github.com>
2025-03-27 14:20:57 +0900

    leet:: bitwise: retry counting_bits

--- leet/bitwise/counting_bits.js
+++ leet/bitwise/counting_bits.js
@@ -2,16 +2,24 @@
  * @param {number} n
  * @return {number[]}
  */
+// var countBits = function (n) {
+//     const arr = [];
+//     for (let i = 0; i < n + 1; i++) {
+//         let num = i;
+//         let count = 0;
+
+//         while (num > 0) {
+//             count = count + (num & 1);
+//             num = num >> 1;
+//         }
+//         arr.push(count);
+//         count = 0;
+//     }
+//     return arr;
+// };
+
 var countBits = function (n) {
-	const dp = Array.from({ length: n + 1 }).fill(0);
-
-	for (let i = 1; i <= n; i++) dp[i] = dp[i >> 1] + (i & 1);
-
-	return dp;
+    const dp = Array(n + 1).fill(0);
+    for (let i = 0; i < n + 1; i++) dp[i] = dp[i >> 1] + (i & 1);
+    return dp;
 };
-
-/*
-   https://leetcode.com/problems/counting-bits/description/?envType=study-plan-v2&envId=leetcode-75
-1. this is more like a dp problem.
-2. bitwise operation to determine if a number is odd or even.
- */
