57a0a8bb60195f176b786e23d949f77459cf305c
hongsoohyuk <63241308+hong-soohyuk@users.noreply.github.com>
2025-04-16 21:52:46 +0900

    leet:: graph: number-of-provinces

---
+++ leet/graph/number-of-provinces.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[][]} isConnected
+ * @return {number}
+ */
+
+/*
+1 0 0 1
+0 1 1 0
+0 1 1 1
+1 0 1 1
+ */
+var findCircleNum = function (isConnected) {
+    const cityNum = isConnected.length;
+    const visited = Array.from({ length: cityNum }, () => false);
+    let province = 0;
+
+    const visitCity = (index) => {
+        visited[index] = true;
+        for (let i = 0; i < cityNum; i++) {
+            if (isConnected[index][i] && !visited[i])
+                visitCity(i);
+        }
+    }
+    for (let i = 0; i < cityNum; i++) {
+        if (!visited[i]) {
+            province++;
+            visitCity(i);
+        }
+    }
+    return province;
+};
+/*
+https://leetcode.com/problems/number-of-provinces/?envType=study-plan-v2&envId=leetcode-75
+*/
