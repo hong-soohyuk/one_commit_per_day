af48cb9a8b50805d0bdd188bf7a1ac36d4f2b715
hongsoohyuk <63241308+hong-soohyuk@users.noreply.github.com>
2025-07-28 21:15:38 +0900

    leet:: greedy: Container With Most Water

---
+++ leet/greedy/container-with-most-water.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let max = 0;
+
+    while (left < right) {
+        max = Math.max(max, (right - left) * Math.min(height[left], height[right]));
+        if (height[left] < height[right]) left++;
+        else right--;
+    }
+    return max;
+};
+
+//https://leetcode.com/problems/container-with-most-water/?envType=problem-list-v2&envId=greedy
